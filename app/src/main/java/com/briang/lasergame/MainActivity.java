4efb2622b02d86d2d31ebf126127df43ef86bf54
BrianGorter078 <briangorter@hotmail.com>
2016-05-30 22:00:21 +0200

    First version

---
+++ app/src/main/java/com/briang/lasergame/MainActivity.java
@@ -0,0 +1,21 @@
+package com.briang.lasergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar mActionBarToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setTitle("LaserGame");
+    }
+
+}
