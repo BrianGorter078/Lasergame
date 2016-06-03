e09c17a67f90a3a8a1c695dbbd11a95d986634ec
Brian Gorter <Briangorter@hotmail.com>
2016-06-03 15:17:03 +0200

    rip wesley

--- app/src/main/java/com/briang/lasergame/MainActivity.java
+++ app/src/main/java/com/briang/lasergame/MainActivity.java
@@ -1,12 +1,30 @@
 package com.briang.lasergame;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar mActionBarToolbar;
+    private Toolbar mActionBarToolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TextView toolbarTitle;
+    private int[] tabIcons = {
+            R.drawable.ic_home,
+            R.drawable.ic_settings
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +33,88 @@ public class MainActivity extends AppCompatActivity {
 
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mActionBarToolbar);
-        getSupportActionBar().setTitle("LaserGame");
+//        getSupportActionBar().setTitle("Home");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0)
+                {
+                    toolbarTitle.setText("Home");
+                }
+                else {
+                    toolbarTitle.setText("Stats");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        setupTabIcons();
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Home(), "One");
+        adapter.addFragment(new Stats(), "TWO");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            System.out.println(position);
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+        }
+
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+////            return mFragmentTitleList.get(position);
+//        }
+    }
 }
